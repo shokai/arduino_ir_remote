df7cfd0c27db336d3cf23967a1558d7052d45084
Sho Hashimoto <hashimoto@shokai.org>
2013-07-22 07:47:57 +0900

    config.yml -> data.yml

--- libs/config.rb
+++ libs/config.rb
@@ -2,5 +2,5 @@ require 'hashie'
 require 'yaml'
 
 module Ir
-  Config = Hashie::Mash.new YAML::load File.open(File.expand_path '../config.yml', File.dirname(__FILE__)).read
+  DATA = Hashie::Mash.new YAML::load File.open(File.expand_path '../data.yml', File.dirname(__FILE__)).read
 end
