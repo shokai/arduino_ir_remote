220ece0d0243788a32c1d0b8eb07c3c5f054932a
Sho Hashimoto <hashimoto@shokai.org>
2013-07-22 09:11:01 +0900

    store logs

--- WebUI/controllers/main.rb
+++ WebUI/controllers/main.rb
@@ -1,6 +1,7 @@
 io = Sinatra::RocketIO
 raise ArgumentError, 'please set ENV["ARDUINO"]' unless ENV["ARDUINO"]
 ir = Ir::Remote.new ENV["ARDUINO"]
+logs = []
 
 get '/' do
   haml :index
@@ -12,6 +13,9 @@ end
 
 io.on :connect do |client|
   $logger.info "new client <#{client}>"
+  logs.each do |log|
+    io.push :log, log, :to => client.session
+  end
 end
 
 io.on :disconnect do |client|
@@ -23,8 +27,13 @@ io.on :ir_write do |name|
     $logger.info "[#{name}] is not Ir::DATA"
     io.push :log, "[#{name}] is not Ir::DATA"
   else
-    $logger.info "[#{name}]"
-    io.push :log, "[#{name}]"
+    log = "[#{name}] - #{Time.now}"
+    $logger.info log
+    io.push :log, log
+    logs.push log
+    while logs.size > 1000
+      logs.shift
+    end
     ir.write Ir::DATA[name]
   end
 end
